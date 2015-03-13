585165bc663b38dc1120fb9dd2dd9bcb4f865734
Steve Aschwanden <steve.aschwanden@students.unibe.ch>
2015-03-13 11:00:04 +0100

    setup cancan and create ability and role model

--- Gemfile
+++ Gemfile
@@ -37,6 +37,7 @@ gem 'less-rails'
 gem 'twitter-bootstrap-rails'
 gem 'kaminari'
 gem 'devise'
+gem 'cancan'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150302134906) do
+ActiveRecord::Schema.define(version: 20150313095810) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -76,6 +76,13 @@ ActiveRecord::Schema.define(version: 20150302134906) do
   end
 
   add_index "ratings", ["task_id"], name: "index_ratings_on_task_id", using: :btree
+
+  create_table "roles", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.string   "title"
