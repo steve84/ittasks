dfff8a1ef0b2721ff3b3c68b4311f7117069d0d0
Steve Aschwanden <steve.aschwanden@students.unibe.ch>
2015-03-30 18:10:01 +0200

    create multiple users, tasks and offers

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,3 +19,45 @@ Role.create(:name => 'User', :description => 'Principal')
 Role.create(:name => 'Abo1', :description => 'Students/RAV')
 Role.create(:name => 'Abo2', :description => 'Freelancer')
 Role.create(:name => 'Abo3', :description => 'Companies')
+
+user = User.new
+user.email = 'admin@ittask.com'
+user.password = '12345678'
+user.password_confirmation = '12345678'
+user.role_id = 1
+user.name = 'Admin'
+user.location = Location.new
+user.location.update(:street => 'Golden Gate', :plz => '1234', :city => 'San Francisco', :country => 'USA')
+user.save!
+
+user = User.new
+user.email = 'user@ittask.com'
+user.password = '12345678'
+user.password_confirmation = '12345678'
+user.role_id = 2
+user.name = 'User'
+user.location = Location.new
+user.location.update(:street => 'Bahnhofstrasse', :plz => '8000', :city => 'Zürich', :country => 'Switzerland')
+user.save!
+
+for i in 1..10
+	user = User.new
+	user.email = 'abo' + i.to_s() + '@ittask.com'
+	user.password = '12345678'
+	user.password_confirmation = '12345678'
+	user.role_id = 3
+	user.name = 'Abo ' + i.to_s()
+	user.location = Location.new
+	user.location.update(:street => 'Kirchstrasse', :plz => '2540', :city => 'Grenchen', :country => 'Schweiz')
+	user.save!
+end
+
+for i in 1..100
+	Task.create(:title => 'Task '+i.to_s(), :description => 'Description '+i.to_s(), :principal_id => 1 + rand(10), :start => Time.now, :end => Time.now)
+end
+
+for i in 1..100
+	for j in 3..10
+		Offer.create(:remark => 'Remark '+i.to_s(), :amount => rand(1000), :task_id => i, :user_id => j)
+	end
+end
